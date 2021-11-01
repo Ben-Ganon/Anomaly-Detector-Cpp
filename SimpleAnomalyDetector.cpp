54771b10ba6e67d84ef2773a684c49355ddd6711
Sagiv Antebi <sagiva13@gmail.com>
2021-11-01 16:10:30 +0200

    learnNormal - almost done

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -3,30 +3,49 @@
 #include "anomaly_detection_util.h"
 
 SimpleAnomalyDetector::SimpleAnomalyDetector() {
-	//TODO Auto-generated constructor stub
+    //TODO Auto-generated constructor stub
     this->cf = new vector<correlatedFeatures>;
 
 }
 
 SimpleAnomalyDetector::~SimpleAnomalyDetector() {
-	//TODO Auto-generated destructor stub
-    //this->cf.clear();
+    //TODO Auto-generated destructor stub
+    this->cf.clear();
 }
 
 
-void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
-	//TODO Auto-generated destructor stub
-    for(int i = 0; i < ts.numColumns(); i++) {
+void SimpleAnomalyDetector::learnNormal(const TimeSeries &ts) {
+    //TODO Auto-generated destructor stub
+    float m, p;
+    int c;
+    for (int i = 0; i < ts.numColumns(); i++) {
+        m = 0;
+        c = -1;
         for (int j = i + 1; j < ts.numColumns(); j++) {
-            pearson(convertVector(ts.getColumn(i)), convertVector(ts.getColumn(j)), ts.getColumn(i).size());
+            p = pearson(convertVector(ts.getColumn(i)), convertVector(ts.getColumn(j)), ts.getColumn(i).size());
+            if (p > m) {
+                m = p;
+                c = j;
+            }
+        }
+        if (1 != c) {
+            correlatedFeatures correlatedFeature = new correlatedFeatures();
+            correlatedFeature.corrlation = m;
+            //need to change the function operation in timeSeries
+            correlatedFeature.feature1 = ts.getCell(0, i);
+            correlatedFeature.feature2 = ts.getCell(0, c);
+            //need to add to those two:
+//            correlatedFeature.lin_reg = ;
+//            correlatedFeature.threshold = ;
+            cf->push_back()
         }
 
 
     }
 }
 
-vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries& ts){
-	//TODO Auto-generated destructor stub
+vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries &ts) {
+    //TODO Auto-generated destructor stub
 }
 
 /**
