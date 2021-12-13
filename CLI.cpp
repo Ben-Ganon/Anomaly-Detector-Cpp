d2bbd82971e1d7a596a44199c39266555a2feeb1
Sagiv Antebi <sagiva13@gmail.com>
2021-12-13 13:29:32 +0200

    menu in CLI is done - need to implement the TODO

--- CLI.cpp
+++ CLI.cpp
@@ -12,7 +12,9 @@ void CLI::start() {
     // the user choice
     char choice;
     // the csv paths
-    string csvTrain, csvTest;
+    string csvTrain, csvTest, localAnomFiles;
+    // the csv threshold
+    float threshold = 0.9;
     // indicates if the loop need to keep runs
     bool run = true;
     while (run) {
@@ -20,10 +22,12 @@ void CLI::start() {
                 << "Welcome to the Anomaly Detection Server. Please choose an option: \n1. upload a time series csv"
                    " file \n2. algorithm settings \n3. detect anomalies \n4. display results \n5. upload anomalies "
                    "and analyze results \n6. exit"
-                << endl
+                << endl;
         cin >> choice;
         switch (choice) {
+            // 1 - upload a time series csv file
             case '1':
+                //TODO: replace the command to read the client csv
                 cout << "Please upload your local train CSV file." << endl;
                 // insert the paths given by the user
                 cin >> csvTrain
@@ -34,10 +38,42 @@ void CLI::start() {
                 cout << "Upload complete." << endl;
                 //TODO: need to implement here the command that sending the given csv from the client to the server
                 break;
+            // 2 - algorithm settings
             case '2':
-                float currentThreshold;
-                //need to get it from the given csv
-                cout << "The current correlation threshold is";
+                cout << "The current correlation threshold is " << threshold << endl;
+                do {
+                    cout << "Type a new threshold" << endl;
+                    cin >> threshold;
+                } while (threshold > 1 && threshold < 0);
+                break;
+            // 3 - detect anomalies
+            case '3':
+                //TODO: run the hybrid anomaly detection
+                cout << "anomaly detection complete." << endl;
+                break;
+            // 4 - display results
+            case '4':
+                //TODO: need to initialize those variables
+                float timeStep;
+                string description;
+                // the printing method given by the instructor
+                cout << timeStep << "\t" << description << endl;
+                cout << "Done." << endl;
+                break;
+            // 5 - upload anomalies
+            case '5':
+                float truePositiveRate, falsePositiveRate;
+                //TODO: replace the command to read the client csv
+                cout << "Please upload your local train CSV file." << endl;
+                // insert the paths given by the user
+                cin >> localAnomFiles
+                cout << "Upload complete." << endl;
+                //TODO: do here all the calculations
+                cout << "True Positive Rate: " << truePositiveRate << endl;
+                cout << "False Positive Rate: " << falsePositiveRate << endl;
+            case '6':
+                run = false;
+                break;
             default:
                 cout << "Invalid choice!" << endl;
 
