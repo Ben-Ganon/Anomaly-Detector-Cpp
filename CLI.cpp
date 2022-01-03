398c84f44bf351934c1be9b7bd6e9c7b2219f15d
Sagiv Antebi <sagiva13@gmail.com>
2022-01-03 11:04:30 +0200

    ex5 - works from here

--- CLI.cpp
+++ CLI.cpp
@@ -7,84 +7,38 @@
 
 CLI::CLI(DefaultIO *dio) {
     this->dio = dio;
+    commands.push_back(new UploadCSV(dio));
+    commands.push_back(new Settings(dio));
+    commands.push_back(new Detect(dio));
+    commands.push_back(new Results(dio));
+    commands.push_back(new UploadAnom(dio));
+    commands.push_back(new Exit(dio));
 }
 
 void CLI::start() {
-    // the user choice
-    char choice;
-    // the csv paths
-    string csvTrain, csvTest, localAnomFiles;
-    // the csv threshold
-    float threshold = 0.9;
-    // indicates if the loop need to keep runs
-    bool run = true;
-    while (run) {
-        cout
-                << "Welcome to the Anomaly Detection Server. Please choose an option: \n1. upload a time series csv"
-                   " file \n2. algorithm settings \n3. detect anomalies \n4. display results \n5. upload anomalies "
-                   "and analyze results \n6. exit"
-                << endl;
-        cin >> choice;
-        switch (choice) {
-            // 1 - upload a time series csv file
-            case '1':
-                //TODO: replace the command to read the client csv
-                cout << "Please upload your local train CSV file." << endl;
-                // insert the paths given by the user
-                cin >> csvTrain;
-                cout << "Upload complete." << endl;
-                cout << "Please upload your local test CSV file." << endl;
-                // insert the paths given by the user
-                cin >> csvTest;
-                cout << "Upload complete." << endl;
-                //TODO: need to implement here the command that sending the given csv from the client to the server
-                break;
-            // 2 - algorithm settings
-            case '2':
-                cout << "The current correlation threshold is " << threshold << endl;
-                do {
-                    cout << "Type a new threshold" << endl;
-                    cin >> threshold;
-                } while (threshold > 1 && threshold < 0);
-                break;
-            // 3 - detect anomalies
-            case '3':
-                //TODO: run the hybrid anomaly detection
-                cout << "anomaly detection complete." << endl;
-                break;
-            // 4 - display results
-            case '4':
-                //TODO: need to initialize those variables
-                float timeStep;
-                string description;
-                // the printing method given by the instructor
-                cout << timeStep << "\t" << description << endl;
-                cout << "Done." << endl;
-                break;
-            // 5 - upload anomalies
-            case '5':
-                float truePositiveRate, falsePositiveRate;
-                //TODO: replace the command to read the client csv
-                cout << "Please upload your local train CSV file." << endl;
-                // insert the paths given by the user
-                cin >> localAnomFiles;
-                cout << "Upload complete." << endl;
-                //TODO: do here all the calculations
-                cout << "True Positive Rate: " << truePositiveRate << endl;
-                cout << "False Positive Rate: " << falsePositiveRate << endl;
-            case '6':
-                run = false;
-                break;
-            default:
-                cout << "Invalid choice!" << endl;
-
-
+    SharedState sharedState;
+    int index = -1;
+    while (index != 5) {
+        dio->write("Welcome to the Anomaly Detection Server.\n");
+        dio->write("Please choose an option:\n");
+        for (size_t i = 0; i < commands.size(); i++) {
+            string s("1.");
+            s[0]=((char)(i+1+'0'));
+            dio->write(s);
+            dio->write(commands[i]->description + "\n");
         }
+        string input = dio->read();
+        index = input[0] - '0' - 1;
+        if (index >= 0 && index <= 6)
+            commands[index]->execute(&sharedState);
     }
-
 }
 
 
 CLI::~CLI() {
+    for (size_t i = 0; i < commands.size(); i++) {
+        delete commands[i];
+    }
 }
+
 
--- HybridAnomalyDetector.h
+++ HybridAnomalyDetector.h
@@ -7,14 +7,24 @@
 #include "minCircle.h"
 #include <vector>
 
-class HybridAnomalyDetector:public SimpleAnomalyDetector {
+class HybridAnomalyDetector : public SimpleAnomalyDetector {
+private:
+    float threshold;
 public:
-	HybridAnomalyDetector();
+    HybridAnomalyDetector();
+
+    void setCorrelationThreshold(float threshold);
+
     void HybridLearner(const TimeSeries &ts, float m, int i, int index);
+
     void learnNormal(const TimeSeries &ts) override;
+
     std::vector<AnomalyReport> detect(const TimeSeries &ts) override;
-    std::vector<Point> getPoints(std::vector<float> v1, std:: vector<float> v2);
-    void hybridDetection(float threshold, Point p, correlatedFeatures cf, int timeStep, std::vector<AnomalyReport>* anomalies);
+
+    std::vector<Point> getPoints(std::vector<float> v1, std::vector<float> v2);
+
+    void hybridDetection(float threshold, Point p, correlatedFeatures cf, int timeStep,
+                         std::vector<AnomalyReport> *anomalies);
 };
 
 #endif /* HYBRIDANOMALYDETECTOR_H_ */
